9d1cfce6861bf37fc053cf9925210b09622ac1eb
salehaTanveer <saleha.zia900@gmail.com>
2023-03-31 15:12:04 +0500

    Swagger integration

--- src/app.py
+++ src/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_restx import Api
+
 
 
 def create_app():
     app = Flask(__name__)
+
+    api = Api(app,
+              title='Swagger with flask',
+              version='1.0',
+              description='A test project with flask-restx.'
+              )
+
+    from .api.v1.routes.initial_routes import hello_world
+    api.add_namespace(hello_world)
 
     @app.route('/')
     def index():
